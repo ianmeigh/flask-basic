c124f4adff4935837be8a93809e4c25b62999e52
ianmeigh <ianmeigh@googlemail.com>
2022-05-21 22:13:00 +0100

    feat: use .env file for secrets

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 from os import environ, path
+from dotenv import load_dotenv
 
 # https://hackersandslackers.com/configure-flask-applications/
+
+# https://www.geeksforgeeks.org/__file__-a-special-variable-in-python/
+# https://dev.to/jakewitcher/using-env-files-for-environment-variables-in-python-applications-55a1
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
 
 
 class Config:
     """Base config"""
 
+    SECRET_KEY = environ.get("SECRET_KEY")
     HOST = (environ.get("IP", "0.0.0.0"),)
     PORT = (int(environ.get("PORT", "5000")),)
 
