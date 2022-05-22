737f788d524b453c9d776641b23d1d4c3c114bcc
ianmeigh <ianmeigh@googlemail.com>
2022-05-22 19:56:00 +0100

    chore: update app to use production config settings

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash
 from datetime import datetime
 
 app = Flask(__name__)
-app.config.from_object("config.DevConfig")
+app.config.from_object("config.ProdConfig")
 
 
 @app.route("/")
